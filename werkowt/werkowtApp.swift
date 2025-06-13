f4fddc4aa40c02fda03dc5c6201379c4e4fb34eb
Chris <christopherdoyle967@gmail.com>
2025-06-13 19:08:42 +0100

    Initial Commit

---
+++ werkowt/werkowtApp.swift
@@ -0,0 +1,17 @@
+//
+//  werkowtApp.swift
+//  werkowt
+//
+//  Created by Christopher Doyle on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct werkowtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
